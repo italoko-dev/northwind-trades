379187c9048f760406a0d7c3ab78544b0909c623
ITALO PIOVAN <italo_piovan@hotmail.com>
2024-06-15 16:28:46 -0300

    Rename of some fields stg models.

--- dbt_northwind_trades/models/staging/erp_northwind_trades/stg_categories.sql
+++ dbt_northwind_trades/models/staging/erp_northwind_trades/stg_categories.sql
@@ -3,8 +3,8 @@ with
         select 
             cast(category_id as int) as category_id
             , cast(category_name as varchar) as category_name
-            , cast(description as varchar) as description
-            -- , picture 
+            , cast(description as varchar) as category_description
+            , cast(picture as varchar) as category_picture
         from {{ source('erp_northwind_trades', 'categories') }}
     )
 select *  
